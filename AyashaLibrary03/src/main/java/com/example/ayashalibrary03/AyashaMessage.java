a4e06d5ba02c5792f787899c94afecabf96824c3
vaibhav <vaibhavg7005@gmail.com>
2023-07-27 22:43:26 +0530

    used rxjava dependency

--- AyashaLibrary03/src/main/java/com/example/ayashalibrary03/AyashaMessage.java
+++ AyashaLibrary03/src/main/java/com/example/ayashalibrary03/AyashaMessage.java
@@ -3,7 +3,17 @@ package com.example.ayashalibrary03;
 import android.content.Context;
 import android.widget.Toast;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleObserver;
+
 public class AyashaMessage {
+    Single res = new Single() {
+        @Override
+        protected void subscribeActual(@NonNull SingleObserver observer) {
+
+        }
+    };
     public static void s (Context context) {
         Toast.makeText(context,"hey",Toast.LENGTH_SHORT).show();
     }
